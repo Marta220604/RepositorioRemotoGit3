639da6eeccdbab9377ba092c52ae4dbfbe9687e1
alumno <alumno@alumno-Lenovo-V14-G5-IRL>
2026-02-26 12:59:42 +0100

    commit 2 con rama1

--- Proyecto3/src/principal3/Clase3.java
+++ Proyecto3/src/principal3/Clase3.java
@@ -4,6 +4,10 @@ public class Clase3 {
 
 	public static void main(String[] args) {
 		System.out.print("1 mensaje");
+		
+		System.out.print("2 mensaje");
+		
+		System.out.print("3 mensaje");
 
 	}
 
